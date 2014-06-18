8d3573955786d38a9506b2c19bc50c052b223271
shinya <shinya.ore@gmail.com>
2014-06-19 02:01:46 +0900

    Initial Commit

---
+++ karuta/KLTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KLTAppDelegate.h
+//  karuta
+//
+//  Created by shinya on 2014/06/19.
+//  Copyright (c) 2014年 shinya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KLTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ karuta/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  karuta
+//
+//  Created by shinya on 2014/06/19.
+//  Copyright (c) 2014年 shinya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KLTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KLTAppDelegate class]));
+    }
+}
